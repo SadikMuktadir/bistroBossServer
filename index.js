3073cecbb5f720179ddbf95062ec503d3afb749d
SadikMuktadir <sadikmuktadir2.0@gmail.com>
2023-12-16 11:22:04 +0600

    cart collection created

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ async function run() {
     // Browser Collection
     const menuCollection = client.db("bistroBoss").collection("menu");
     const reviewCollection = client.db("bistroBoss").collection("review");
+    const cartCollection = client.db("bistroBoss").collection("cart");
 
     // MongoDB to UI
     // menuCollection
@@ -43,6 +44,12 @@ async function run() {
       const result = await reviewCollection.find().toArray();
       res.send(result);
     });
+    // cartCollection
+    app.post("/carts",async (req,res)=>{
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
