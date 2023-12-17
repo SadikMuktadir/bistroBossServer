e5ae5788a365c30bb4e2a4917605c049aa16f6eb
SadikMuktadir <sadikmuktadir2.0@gmail.com>
2023-12-17 17:47:53 +0600

    cart delete option created

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 // MongoDB Connect
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k6zwazt.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -56,6 +56,12 @@ async function run() {
       const result = await cartCollection.find(query).toArray();
       res.send(result);
     });
+    app.delete("/carts/:id",async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
