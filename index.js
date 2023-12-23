f21042808c7b4ce262e8e31579fd38cdd67a080f
SadikMuktadir <sadikmuktadir2.0@gmail.com>
2023-12-24 00:56:14 +0600

    menu get and delete option is completed

--- index.js
+++ index.js
@@ -96,6 +96,12 @@ async function run() {
       const result = await menuCollection.insertOne(item);
       res.send(result);
     });
+    app.delete("/menu/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    });
     // reviewCollection
     app.get("/review", async (req, res) => {
       const result = await reviewCollection.find().toArray();
